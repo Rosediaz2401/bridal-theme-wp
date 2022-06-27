6ed7e71b002e5db063b28ef347ecaee123127bf0
rosediaz <rosa.diaz.aguilar@gmail.com>
2022-06-27 15:49:36 -0500

    actualizacion form en seccion de contact

--- page.php
+++ page.php
@@ -345,42 +345,6 @@
         </div>
 <?php endif; ?>
 
-<?php if (is_page('91') ):?>
-    <div class="main-container testimonial-main-container">
-            <div class="contact-section">
-                <div class="maps-section">
-                    <div class="testimonial">
-                        <h2 class="title testimonial-title testimonial">Contact us!</h2>
-                        <p class="contact">Tel:(322) 297 0218</p>
-                        <p class="contact">Mail: hello@bridalphotography.mx</p>
-                    </div>
-                    <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3732.0052261420096!2d-105.30404448473716!3d20.710012386171886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x842146b80722d69f%3A0x6dcd6e360db88018!2sMarival%20Emotions%20Resort%20%26%20Suites!5e0!3m2!1ses-419!2smx!4v1654900279715!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                    <img class="img-map" src="/img/viñetas-1.png" alt="vinetas">
-                </div>
-                <div class="main-container contact-container">
-                <form action="" method="post">
-                    <label class="contact-info" for="name">Name (required)</label>
-                    <input class="contact-input" type="text" id="name" name="name">
-                    <p class="your-info">Your Name</p>
-
-                    <label class="contact-info" for="email">Mail (required)</label>
-                    <input class="contact-input" type="email" id="email" name="email">
-                    <p class="your-info">Your Email</p>
-
-                    <label class="contact-info" for="subject">Subject (required)</label>
-                    <input class="contact-input" type="text" id="subject" name="subject">
-                    <p class="your-info">Your Subject</p>
-
-                    <label class="contact-info" for="subject">Message (required)</label>
-                    <input class="contact-input msj-input" type="text" id="message" name="message">
-                    <p class="your-info">Your Message</p>
-                </form>
-                <div class="form-btn">
-                    <button class="contact-btn">Submit</button>
-                </div>
-                </div>
-            </div>
-<?php endif; ?>
 
 <?php if (is_page('23') ):?>
     <div class="main-container testimonial-main-container">
@@ -469,6 +433,11 @@
                 </div>
             </div>
         </div>
+<?php endif; ?>
+
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+the_content();
+endwhile; else: ?>
 <?php endif; ?>
 
 
